8f0388eaf7cf804ff1eb3466c99da339a74253a6
Andy Wang <andywang0321@gmail.com>
2024-08-20 16:10:26 -0700

    tried new model

--- model/model.py
+++ model/model.py
@@ -9,46 +9,58 @@ class CatClassifier(nn.Module):
     - normal
     - obese
     '''
-    def __init__(self):
+    def __init__(self, dropout=0.5):
         super().__init__()
         self.maxpool = nn.MaxPool2d(
             kernel_size=2
         )
         self.conv1 = nn.Conv2d(
             in_channels=3,
-            out_channels=16,
-            kernel_size=3
-        )
-        self.conv2 = nn.Conv2d(
-            in_channels=16,
             out_channels=32,
-            kernel_size=3
+            kernel_size=7
         )
-        self.conv3 = nn.Conv2d(
+        self.batchnorm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(
             in_channels=32,
-            out_channels=16,
+            out_channels=64,
+            kernel_size=5
+        )
+        self.batchnorm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
             kernel_size=3
         )
+        self.batchnorm3 = nn.BatchNorm2d(128)
         self.flatten = nn.Flatten()
-        self.linear1 = nn.Linear(14400, 256)
-        self.linear2 = nn.Linear(256, 3)
+        self.linear1 = nn.Linear(107648, 512)
+        self.linear2 = nn.Linear(512, 128)
+        self.linear3 = nn.Linear(128, 3)
+        self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.batchnorm1(x)
         x = F.relu(x)
         x = self.maxpool(x)
 
         x = self.conv2(x)
+        x = self.batchnorm2(x)
         x = F.relu(x)
         x = self.maxpool(x)
 
         x = self.conv3(x)
+        x = self.batchnorm3(x)
         x = F.relu(x)
         x = self.maxpool(x)
         
         x = self.flatten(x)
         x = self.linear1(x)
         x = F.relu(x)
+        x = self.dropout(x)
         x = self.linear2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.linear3(x)
         return F.softmax(x)
 
