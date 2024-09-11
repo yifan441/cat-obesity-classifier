d7dcbc59d42e2ea21a45db78a9272461805c2fbd
Yifan Tang <yifant@g.ucla.edu>
2024-09-11 16:03:42 -0700

    Removed unnecessary files

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,4 +1,6 @@
+import * as React from "react";
 import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function Index() {
   return (
